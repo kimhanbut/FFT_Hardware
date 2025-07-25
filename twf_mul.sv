b74bd35c8b21b3ec9996e67af87fa879bd267594
kimhanbut <bigfriend28@naver.com>
2025-07-25 19:17:14 +0900

    Create twf_mul.sv

---
+++ twf_mul.sv
@@ -0,0 +1,19 @@
+module twiddle_mul (
+    input  logic signed [13:0] data_re_in [15:0],
+    input  logic signed [13:0] data_im_in [15:0],
+    input  logic signed  [8:0] twf_re_in  [15:0],
+    input  logic signed  [8:0] twf_im_in  [15:0],
+
+    output logic signed [27:0] data_re_out [15:0],
+    output logic signed [27:0] data_im_out [15:0]
+);
+
+    always_comb begin
+        for (int i = 0; i < 16; i++) begin
+            // 복소수 곱셈: (a+jb)*(c+jd) = (ac - bd) + j(ad + bc)
+            data_re_out[i] = data_re_in[i] * twf_re_in[i] - data_im_in[i] * twf_im_in[i];
+            data_im_out[i] = data_re_in[i] * twf_im_in[i] + data_im_in[i] * twf_re_in[i];
+        end
+    end
+
+endmodule
