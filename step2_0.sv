bae4160805d5d527ecf060a0fed033edd80667d8
Kim EulJoong <127865678+KimEulJoong@users.noreply.github.com>
2025-08-01 20:07:35 +0900

    Update step2_0.sv

--- step2_0.sv
+++ step2_0.sv
@@ -4,13 +4,11 @@ module step2_0 (
     input  logic               clk,
     input  logic               rstn,
     input  logic               din_valid,
-    input  logic signed [12:0] din_r [0:15],  // 512개 입력 (Q12)
+    input  logic signed [12:0] din_r [0:15], 
     input  logic signed [12:0] din_i [0:15],
 
-    output logic signed [13:0] dout_add_r[0:15], // A+B
-    output logic signed [13:0] dout_add_i[0:15],
-    output logic signed [13:0] dout_sub_r[0:15], // A−B
-    output logic signed [13:0] dout_sub_i[0:15]
+    output logic signed [13:0] dout_r[0:15], 
+    output logic signed [13:0] dout_i[0:15]
 );
 
     logic local_valid;
@@ -38,10 +36,8 @@ module step2_0 (
         .input_real(din_r),
         .input_imag(din_i),
         .valid_out(), // 사용하지 않음
-        .output_real_add(dout_add_r),
-        .output_imag_add(dout_add_i),
-        .output_real_diff(dout_sub_r),
-        .output_imag_diff(dout_sub_i)
+        .output_real(dout_r),
+        .output_imag(dout_i)
     );
 
 endmodule
