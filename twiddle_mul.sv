9cf0a2bfb83bc1d248d34f1d7b8534524242a855
kimhanbut <bigfriend28@naver.com>
2025-08-03 21:39:27 +0900

    Golden twiddle_mul.sv

--- twiddle_mul.sv
+++ twiddle_mul.sv
@@ -1,20 +1,25 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module twiddle_mul (
-    input  logic signed [13:0] data_re_in [15:0],
-    input  logic signed [13:0] data_im_in [15:0],
-    input  logic signed  [8:0] twf_re_in  [15:0],
-    input  logic signed  [8:0] twf_im_in  [15:0],
+module twiddle_mul #(
+    parameter IN_DATA_W = 14,
+    parameter OUT_DATA_W = 23,
+    parameter DATA_SIZE = 16
+) (
+    input logic signed [IN_DATA_W -1:0] data_re_in[0:DATA_SIZE-1],
+    input logic signed [IN_DATA_W -1:0] data_im_in[0: DATA_SIZE-1],
+    input logic signed [ 8:0] twf_re_in [0: DATA_SIZE - 1],
+    input logic signed [ 8:0] twf_im_in [0: DATA_SIZE -1],
 
-    output logic signed [22:0] data_re_out [15:0],
-    output logic signed [22:0] data_im_out [15:0]
+    output logic signed [OUT_DATA_W - 1:0] data_re_out[0: DATA_SIZE -1],
+    output logic signed [OUT_DATA_W - 1:0] data_im_out[0: DATA_SIZE -1]
 );
 
     always_comb begin
-        for (int i = 0; i < 16; i++) begin
+        for (int i = 0; i < DATA_SIZE; i++) begin
             // 복소수 곱셈: (a+jb)*(c+jd) = (ac - bd) + j(ad + bc)
             data_re_out[i] = data_re_in[i] * twf_re_in[i] - data_im_in[i] * twf_im_in[i];
             data_im_out[i] = data_re_in[i] * twf_im_in[i] + data_im_in[i] * twf_re_in[i];
         end
     end
+
 endmodule
